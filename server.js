a2a22b001c66d8ea04deb7f4f68313901a6818d4
Amal Ahmed <amalmahmoud168@gmail.com>
2024-09-07 01:33:16 +0300

    adjustments

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*',(req,res)=>
+{
+    res.status(200).sendFile(__dirname+'/dist/weatherforcast/index.html');
+})
+
+app.listen(process.env.PORT || 8080);
