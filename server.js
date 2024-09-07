26508132f4e2a37339d4a3a28cadf04f6ab0ab27
Amal Ahmed <amalmahmoud168@gmail.com>
2024-09-07 07:16:19 +0300

    Fix

--- server.js
+++ server.js
@@ -16,3 +16,10 @@ app.get('/api/*', (req, res) => {
   const url = `http://api.geonames.org${req.originalUrl.replace('/api/', '/')}`;
   req.pipe(request(url)).pipe(res);
 });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins or specify a domain
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
