c0a8f9cbe7eca2835f1db34454b3a24435129d63
Amal Ahmed <amalmahmoud168@gmail.com>
2024-09-07 06:34:18 +0300

    Fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/weatherforcast'));
+app.use(express.static(__dirname + '/dist'));
 
 
 app.get('*', (req, res) => {
