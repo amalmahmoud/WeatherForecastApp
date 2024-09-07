a9aaf548599e988ddb64d41cd964b962f16aa284
Amal Ahmed <amalmahmoud168@gmail.com>
2024-09-07 04:57:46 +0300

    prod

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-let express = require('express');
+const express = require('express');
+const path = require('path');
 
-let app = express();
+const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist/weatherforcast')));
 
-app.get('/*',(req,res)=>
-{
-    res.status(200).sendFile(__dirname+'/dist/weatherforcast/index.html');
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/weatherforcast/index.html'));
+});
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
