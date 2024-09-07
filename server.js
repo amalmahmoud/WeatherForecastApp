100c5911c116ce676282d9ee77d5300575138513
Amal Ahmed <amalmahmoud168@gmail.com>
2024-09-07 13:36:04 +0300

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.listen(port, () => {
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins or specify a domain
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
